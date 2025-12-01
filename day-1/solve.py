ea62134ca091994834e5191398e55e904e3ebf21
henrikskog <henrikskog01@gmail.com>
2025-12-01 17:12:20 +0100

    ouf

--- day-1/solve.py
+++ day-1/solve.py
@@ -6,7 +6,7 @@ num_0 = 0
 
 example = [x.strip() for x in """L68
 L30
-R1048
+R48
 L5
 R60
 L55
@@ -15,61 +15,65 @@ L99
 R14
 L82""".split("\n")]
 
-for line in example:
+def a(c, d, t, l):
     prev_curr = curr
     d = line[0]
     # print(line)
     d2 = int(line[1:]) if d == "R" else -int(line[1:])
     # print("d2", d2)
     
-    curr += d2
-    # print("mid", curr)
+    c += d2
+    # print("mid", c)
 
-    if curr < 0:
-        curr = 100 - abs(int(str(curr)[-2:]))
-    if curr == 100:
-        curr = 0
-    if curr > 100:
-        curr = int(str(curr)[-2:])
+    if c < 0:
+        c = 100 - abs(int(str(c)[-2:]))
+    if c == 100:
+        c = 0
+    if c > 100:
+        c = int(str(c)[-2:])
 
-    # print("point", curr)
-
+    # print("point", c)
 
    # task 2:
 
     # startet på prev_curr
     # flyttet oss d2
-    # endte på curr
+    # endte på c
 
-    # hvis d2 < 0 && abs(d2) > curr -> gått forbi 0
-    # hvis d2 > 0 && d2 > (curr-100) > curr -> gått forbi 0
+    # hvis d2 < 0 && abs(d2) > c -> gått forbi 0
+    # hvis d2 > 0 && d2 > (c-100) > c -> gått forbi 0
 
-    print(f"{prev_curr} + {d2}({line}) = {curr}")
+    print(f"{prev_curr} + {d2}({line}) = {c}")
 
     if d2 < 0 and abs(d2) > prev_curr and (prev_curr != 0 or (-d2 > 100)):
         num_times = 1 if abs(d2) < 100 else abs(d2) // 100
         print(f"passed 0 {num_times} times")
-        num_0+=num_times
+        t+=num_times
 
     elif d2 > 0 and d2 > (100-prev_curr) and (prev_curr != 0 or (d2 > 100)):
         num_times = 1 if abs(d2) < 100 else abs(d2) // 100
         print(f"passed 0 {num_times} times")
-        num_0+=num_times
+        t+=num_times
 
     else:
         print("passed 0 0 times")
 
 
-    if curr == 0:
-        num_0+=1
+    if c == 0:
+        t+=1
         print("at 0: yes")
     else:
         print("at 0: no")
 
-    print("num 0s: ", num_0)
+    print("num 0s: ", t)
     print()
 
+    return (c, t)
 
 
+total = 0
+for line in example:
+    (curr, t) = a(curr, line[0], total, line)
+    total += t
 
-print(num_0)
+print(total)
